e335c121c24550ca74574c78f9ae0740e2f3a7fa
Frank Walsh <fxwalsh@wit.ie>
2023-03-13 13:41:56 +0000

    week8

---
+++ topic12/book-1/Untitled-1.py
@@ -0,0 +1,11 @@
+def calculatePixel:
+    i=math.rand()
+    if i<.5
+        return [0,0,0]
+    else    
+        return [0,255,0]
+
+display = [calculatePixel(),calculatePixel(),calculatePixel(),calculatePixel(),calculatePixel(),calculatePixel(),calculatePixel(),calculatePixel(),
+    calculatePixel(),calculatePixel(),calculatePixel(),calculatePixel(),calculatePixel(),calculatePixel(),calculatePixel(),calculatePixel(),
+    calculatePixel(),calculatePixel(),calculatePixel(),calculatePixel(),calculatePixel(),calculatePixel(),calculatePixel(),calculatePixel(),
+    calculatePixel(),calculatePixel(),calculatePixel(),calculatePixel(),calculatePixel(),calculatePixel(),]
