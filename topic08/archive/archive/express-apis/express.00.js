60765ff1f8c17862d59e32257330fcdd348db034
Frank Walsh <fxwalsh@wit.ie>
2023-03-22 17:34:54 +0000

    week9

--- topic08/archive/archive/express-apis/express.00.js
+++ topic08/archive/archive/express-apis/express.00.js
@@ -10,6 +10,12 @@ app.get('/api/dogs', (req, res) => {
   res.end('should return list of dogs')
 });
 
+app.get('/api/dogs/:id', (req, res) => {
+    let id = req.params.id;
+    res.end('should return details of  dog: ' + id)
+  });
+
+
 app.listen(8080, () => {
   console.info('Express listening on port', 8080);
 });
