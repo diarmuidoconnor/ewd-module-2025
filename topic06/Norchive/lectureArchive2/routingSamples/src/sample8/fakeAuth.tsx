3460305f710840184e9dab8271167dc1884676bb
Frank Walsh <fxwalsh@wit.ie>
2024-03-11 13:15:09 +0000

    topic 6

--- topic06/Norchive/lectureArchive2/routingSamples/src/sample8/fakeAuth.tsx
+++
@@ -1,4 +0,0 @@
-export default async (username,password) =>
-  new Promise((resolve) => {
-    setTimeout(() => resolve('2342f2f1d131rf12'), 250);
-  });
--- topic06/Norchive/lectureArchive2/routingSamples/src/sample8/index.tsx
+++
@@ -1,5 +0,0 @@
-// See the following article for a detailed explanation of 
-// this sample's code:
-// https://www.robinwieruch.de/react-router-authentication/
-
-import './version2';
